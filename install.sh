66d5f2cc0041ed166786c1cbeb7bb78537235533
niltonvasques <niltonvasques@gmail.com>
2013-03-20 22:33:49 -0300

    Merge branch 'refs/heads/dev'
    Conflicts:
    	run.sh
    	src/libstr.inc

--- install.sh
+++
@@ -1,9 +0,0 @@
-#!/bin/bash
-
-echo 'Compiling boot0.asm...'
-nasm src/boot0.asm -f bin -o bin/boot0.bin -I src
-echo 'Compiling boot1.asm...'
-nasm -f bin src/boot1.asm -I src/ -o bin/boot1.bin
-echo 'Create floppy image marvin.img...'
-cat bin/boot0.bin bin/boot1.bin > bin/marvin.img
-./run.sh bin/marvin.img
